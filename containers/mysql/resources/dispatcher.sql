35f9efaa6c79a64d670558213c0753902bac0f1f
kevinnguyenai <kevin.nguyen.ai@gmail.com>
2021-09-07 21:41:22 +0700

    fix dispatcher config kamailio.conf

---
+++ containers/mysql/resources/dispatcher.sql
@@ -0,0 +1,3 @@
+INSERT INTO dispatcher VALUES(1,1,'sip:my-kamailio-lab_asterisk1_1:5060',0,12,'rweight=50;weight=50;cc=1;','');
+INSERT INTO dispatcher VALUES(2,1,'sip:my-kamailio-lab_asterisk2_1:5060',0,12,'rweight=50;weight=50;cc=1;','');
+INSERT INTO dispatcher VALUES(3,1,'sip:my-kamailio-lab_asterisk3_1:5060',0,12,'rweight=50;weight=50;cc=1;','');
