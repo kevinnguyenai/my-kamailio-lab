52230ebd76fff549a45cfc4b0645c89bcb50e9f9
kevinnguyenai <kevin.nguyen.ai@gmail.com>
2021-09-11 23:16:25 +0700

    update config dispatch

--- containers/mysql/resources/dispatcher.sql
+++ containers/mysql/resources/dispatcher.sql
@@ -1,3 +1,6 @@
 INSERT INTO dispatcher VALUES(1,1,'sip:my-kamailio-lab_asterisk1_1:5060',0,12,'rweight=50;weight=50;cc=1;','');
 INSERT INTO dispatcher VALUES(2,1,'sip:my-kamailio-lab_asterisk2_1:5060',0,12,'rweight=50;weight=50;cc=1;','');
 INSERT INTO dispatcher VALUES(3,1,'sip:my-kamailio-lab_asterisk3_1:5060',0,12,'rweight=50;weight=50;cc=1;','');
+INSERT INTO dispatcher VALUES(4,1,'sip:172.19.0.3:5060',0,12,'rweight=50;weight=50;cc=1;','');
+INSERT INTO dispatcher VALUES(5,1,'sip:172.19.0.4:5060',0,12,'rweight=50;weight=50;cc=1;','');
+INSERT INTO dispatcher VALUES(6,1,'sip:172.19.0.5:5060',0,12,'rweight=50;weight=50;cc=1;','');
